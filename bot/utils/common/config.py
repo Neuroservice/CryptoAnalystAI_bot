dba6c6e3120b224afd11c6b17934963a2e75eaae
dimak <dima.kulaga5@gmail.com>
2025-02-06 17:54:33 +0300

    новый docker-compose, фикс бэкапов, гитфлоу

--- bot/utils/common/config.py
+++ bot/utils/common/config.py
@@ -15,7 +15,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 REDIS_HOST = os.getenv("DB_HOST")
-REDIS_PORT = 6379
+REDIS_PORT = os.getenv("REDIS_PORT")
 
 S3_URL = os.getenv("S3_URL")
 S3_AWS_STORAGE_BUCKET_NAME = os.getenv("S3_AWS_STORAGE_BUCKET_NAME")
