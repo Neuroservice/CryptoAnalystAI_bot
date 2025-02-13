473c9f6055fcc5fede867de17ce15e3dfcf5b932
dimak <dima.kulaga5@gmail.com>
2025-02-14 00:34:20 +0300

    config fix

--- bot/utils/common/sessions.py
+++ bot/utils/common/sessions.py
@@ -4,7 +4,7 @@ from aiohttp import ClientSession
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
 from sqlalchemy.orm import sessionmaker
 
-from bot.utils.common.config import REDIS_HOST, DB_PASSWORD, REDIS_PORT
+from bot.utils.common.config import REDIS_HOST, REDIS_PORT
 from bot.utils.common.consts import DATABASE_URL
 
 async_engine: AsyncEngine = create_async_engine(
@@ -23,4 +23,4 @@ SessionLocal.configure(bind=async_engine)
 session_local = SessionLocal()
 client_session = ClientSession
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=DB_PASSWORD, db=0, decode_responses=True)
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0, decode_responses=True)
