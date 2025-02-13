61c8f7beafe50b646756c59c486a02e602ea097a
dimak <dima.kulaga5@gmail.com>
2025-02-14 00:26:00 +0300

    config fix

--- bot/utils/common/config.py
+++ bot/utils/common/config.py
@@ -14,8 +14,8 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-REDIS_HOST = os.getenv("DB_HOST")
-REDIS_PORT = 6379
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
 
 S3_URL = os.getenv("S3_URL")
 S3_AWS_STORAGE_BUCKET_NAME = os.getenv("S3_AWS_STORAGE_BUCKET_NAME")
